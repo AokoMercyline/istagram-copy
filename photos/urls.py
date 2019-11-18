ebe7817e819a3d84f4eed584bee77896f726731c
Titus Ouko <titusouko@gmail.com>
2019-11-18 18:41:02 +0300

    Add edit profile url

--- photos/urls.py
+++ photos/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^image/$', views.add_image, name='upload_image'),
     url(r'^profile/$', views.profile_info, name='profile'),
+    url(r'^update/$', views.profile_update, name='update'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
