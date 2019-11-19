e71afe697888886997db1ae9f75337f16f363def
Titus Ouko <titusouko@gmail.com>
2019-11-19 03:52:56 +0300

    Add view function for comments

--- photos/views.py
+++ photos/views.py
@@ -4,12 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Image, Profile,Comments,Likes
+ 
 
-# def index(request):
-   
-#         return render(request, 'istagram/index.html')
-
-# @login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
 def index(request):
     posts = Image.get_all_images()
     profile = Profile.get_all_profiles()
@@ -23,8 +20,8 @@ def index(request):
             comment.save()
         return redirect('index')
     else:
-        form=CommentForm
-    context =  {
+        form=CommentForm()
+        context =  {
         "profile": profile,
         "form": form,
         "posts":posts ,
@@ -32,7 +29,7 @@ def index(request):
     }
     return render(request, 'istagram/index.html', context)
     
-
+@login_required(login_url='/accounts/login/')
 def add_image(request):
         current_user = request.user
         if request.method == 'POST':
@@ -46,12 +43,17 @@ def add_image(request):
                 form = ImageForm()
                 return render(request,'istagram/image.html', {"form":form})
 
+@login_required(login_url='/accounts/login/')                
 def profile_info(request):
-        profile = Profile.objects.first()
+        current_user = request.user
+        follow = len(Follow.objects.followers(users))
+        following = len(Follow.objects.following(users))
+        people_following = Follow.objects.following(request.user)
+        profile = Profile.objects.filter(user=current_user).first()
         posts = request.user.image_set.all()
        
-        return render(request, 'istagram/profile.html', {"images": posts, "profile": profile})
-
+        return render(request, 'istagram/profile.html', {"images": posts, "profile": profile, 'follow':follow, 'following':following,'people_following':people_following})
+@login_required(login_url='/accounts/login/') 
 def profile_update(request):
          current_user = request.user
          if request.method == 'POST':
@@ -64,3 +66,67 @@ def profile_update(request):
          else:
                 form = ProfileForm()
          return render(request,'istagram/profile_update.html',{"form":form})
+
+@login_required(login_url='/accounts/login/') 
+def comment(request,image_id):
+        current_user=request.user
+        image = Image.objects.get(id=image_id)
+        profile_owner = User.objects.get(username=current_user)
+        comments = Comments.objects.all()
+        if request.method == 'POST':
+                form = CommentForm(request.POST, request.FILES)
+                if form.is_valid():
+                        comment = form.save(commit=False)
+                        comment.image = image
+                        comment.comment_owner = current_user
+                        comment.save()
+            
+                       
+                return redirect(index)
+        else:
+                form = CommentForm()
+        return render(request, 'istagram/comment.html',{"comments":comments})
+
+@login_required(login_url='/accounts/login/') 
+def like(request, image_id):
+    current_user = request.user
+    image=Image.objects.get(id=image_id)
+    new_like,created= Likes.objects.get_or_create(user=current_user, image=image)
+    new_like.save()
+
+    return redirect('index')
+
+# def like(request,image_id):
+#   image = Image.objects.get(pk = image_id)
+#   is_liked = False
+#   if image.likes.filter(id = request.user.id).exists():
+#       image.likes.remove(request.user)
+#       is_liked = False
+#   else:
+#       image.likes.add(request.user)
+#       is_liked = True
+#   return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_users = User.objects.filter(username__icontains = search_term)
+        message = f"{search_term}"
+        profile_pic = User.objects.all()
+        return render(request, 'search.html', {'message':message, 'users':searched_users, 'profile_pic':profile_pic})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {'message':message})
+
+def follow(request, user_id):
+    other_user = User.objects.get(id = user_id)
+    follow = Follow.objects.add_follower(request.user, other_user)
+
+    return redirect('index')
+
+def unfollow(request, user_id):
+    other_user = User.objects.get(id = user_id)
+    follow = Follow.objects.remove_follower(request.user, other_user)
+
+    return redirect('index')
