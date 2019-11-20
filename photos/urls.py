56fae15d920b17ea44799429f0beb8e63fbc3f93
Titus Ouko <titusouko@gmail.com>
2019-11-20 22:35:03 +0300

    Add likes view to the photos pattern

--- photos/urls.py
+++ photos/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^follow/(?P<user_id>\d+)', views.follow, name = 'follow'),
     url(r'^unfollow/(?P<user_id>\d+)', views.unfollow, name='unfollow'),
-    url(r'^likes/(\d+)/$', views.like_images,name="likes")
+    url(r'^likes/(\d+)/$', views.like_images,name='likes')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
